85d9cb7be0cb2946d3c1cf60ba77216982f17f5f
hasant23 <h.tuten23@gmail.com>
2022-06-29 11:58:04 +0000

    new

--- selenium-jobs/test_owners_register_headless.py
+++ selenium-jobs/test_owners_register_headless.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from time import sleep
+import random
 import os
-
 # Set chrome options for working with headless mode (no screen)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
@@ -13,25 +11,58 @@ chrome_options.add_argument("disable-dev-shm-usage")
 
 # Update webdriver instance of chrome-driver with adding chrome options
 driver = webdriver.Chrome(options=chrome_options)
-# driver = webdriver.Chrome("/Users/home/Desktop/chromedriver")
+
 # Connect to the application
 APP_IP = os.environ['MASTER_PUBLIC_IP']
 url = "http://"+APP_IP.strip()+":30001/"
-# url = "http://localhost:8080"
 print(url)
 driver.get(url)
-sleep(3)
 owners_link = driver.find_element_by_link_text("OWNERS")
 owners_link.click()
 sleep(2)
-all_link = driver.find_element_by_link_text("ALL")
+all_link = driver.find_element_by_link_text("REGISTER")
 all_link.click()
 sleep(2)
-
-# Verify that table loaded
+# Register new Owner to Petclinic App
+fn_field = driver.find_element_by_name('firstName')
+fn = 'Callahan' + str(random.randint(0, 100))
+fn_field.send_keys(fn)
 sleep(1)
-verify_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "table")))
+fn_field = driver.find_element_by_name('lastName')
+fn_field.send_keys('Clarusway')
+sleep(1)
+fn_field = driver.find_element_by_name('address')
+fn_field.send_keys('Ridge Corp. Street')
+sleep(1)
+fn_field = driver.find_element_by_name('city')
+fn_field.send_keys('McLean')
+sleep(1)
+fn_field = driver.find_element_by_name('telephone')
+fn_field.send_keys('+1230576803')
+sleep(1)
+fn_field.send_keys(Keys.ENTER)
 
-print("Table loaded")
-
+# Wait 10 seconds to get updated Owner List
+sleep(10)
+# Verify that new user is added to Owner List
+if fn in driver.page_source:
+    print(fn, 'is added and found in the Owners Table')
+    print("Test Passed")
+else:
+    print(fn, 'is not found in the Owners Table')
+    print("Test Failed")
 driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
